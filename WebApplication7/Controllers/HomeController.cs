4f1defb046a2d2485c9fae55b093b9170ca36efc
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2023-12-22 22:28:02 +0300

    localization işlemine başlandı cümleler ve valuleri eklenecek

--- WebApplication7/Controllers/HomeController.cs
+++ WebApplication7/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebApplication7.Models;
+using WebApplication7.Services;
 
 namespace WebApplication7.Controllers
 {
@@ -9,14 +11,30 @@ namespace WebApplication7.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private LanguageService _localization;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,LanguageService locolization)
         {
             _logger = logger;
+            _localization = locolization;
         }
+
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions()
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                });
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+
+
         [Authorize(Roles = "User,Admin")]
         public IActionResult Index()
         {
+            ViewBag.Welcome = _localization.Getkey("Welcome").Value;
+            var currentCulture = Thread.CurrentThread.CurrentCulture.Name;
             return View();
         }
         [Authorize(Roles = "Admin")]
