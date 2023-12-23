5f49ca6966a370a681afece33bc9daa3f8e23530
Ahmet Arif <karakullukcuahmetarif@gmail.com>
2023-12-24 02:44:41 +0300

    localization işlemleri

--- WebApplication7/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebApplication7/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -98,6 +98,7 @@ namespace WebApplication7.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
         }
 
 
@@ -114,8 +115,6 @@ namespace WebApplication7.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
-                
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
